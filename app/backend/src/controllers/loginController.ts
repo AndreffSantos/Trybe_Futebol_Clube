359a5254717c5c9a529b8647b50235b56d4acc58
Andre Santos <andrefelipe_santos@outlook.com>
2022-10-01 17:33:08 -0300

    implementa Controller de login.

---
+++ app/backend/src/controllers/loginController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import LoginService from '../services/loginService';
+
+export default class LoginController {
+  constructor(private service: LoginService) {}
+
+  async login(req: Request, res: Response): Promise<Response> {
+    try {
+      const token = await this.service.login(req.body);
+      return res.status(200).json({ token });  
+    } catch (error: any) {
+      return res.status(error.statusCode).json({ error: error.error });
+    }
+  }
+
+  async validate(req: Request, res: Response): Promise<Response> {
+    try {
+      const role = await this.service.validate(req.headers);
+      return res.status(200).json({ role });
+    } catch (error) {
+      return res.status(200).json({ error: error });
+    }
+  }
+}
