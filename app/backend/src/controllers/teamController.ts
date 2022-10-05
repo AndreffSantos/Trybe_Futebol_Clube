7125cf41ba5bf9f6d5f974f4b4e6ca001ee9f3fb
Andre Santos <andrefelipe_santos@outlook.com>
2022-10-05 07:56:09 -0300

    implementa o metodo getById no controller.

--- app/backend/src/controllers/teamController.ts
+++ app/backend/src/controllers/teamController.ts
@@ -12,4 +12,13 @@ export default class TeamController {
       return res.status(error.statusCode).json({ message: error.message });
     }
   }
+
+  async getById(req: Request, res: Response) {
+    try {
+      const user = await this.service.getById(req.params);
+      return res.status(200).json(user);
+    } catch (error) {
+      
+    }
+  }
 }
