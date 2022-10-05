a39bc702d71048a8dca6e95878bd27be8ab8a2fa
Andre Santos <andrefelipe_santos@outlook.com>
2022-10-05 16:43:38 -0300

    implementa tabela de jogos (matches).

---
+++ app/backend/src/database/migrations/20221005112028-create-matches.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        foreignKey: true,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      home_team_goals: Sequelize.INTEGER,
+      away_team: {
+        type: Sequelize.INTEGER,
+        foreignKey: true,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      away_team_goals: Sequelize.INTEGER,
+      in_progress: Sequelize.INTEGER,
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('matchas');
+  }
+};
