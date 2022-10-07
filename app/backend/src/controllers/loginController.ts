ba8f4063b88e09d1bd2347dcbb71d1e4fd8bd60f
Andre Santos <andrefelipe_santos@outlook.com>
2022-10-07 11:54:13 -0300

    refatora o controller de login.

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -15,7 +15,7 @@ export default class LoginController {
 
   async validate(req: Request, res: Response): Promise<Response> {
     try {
-      const role = await this.service.validate(req.headers);
+      const role = this.service.validate(req.headers);
       return res.status(200).json({ role });
     } catch (error: any) {
       return res.status(error.statusCode).json({ message: error.message });
